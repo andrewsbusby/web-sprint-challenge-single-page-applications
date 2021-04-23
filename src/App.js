166fda5ea2de3aa43642a7643e51ef241ae3fdcd
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-04-23 16:48:59 -0600

    added Router path

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
-import {Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Form from './Form';
 import Home from './Home';
 
 
 const App = () => {
   return (
+  <Router>
     <Switch>
       <Route path='/' exact component={Home}/>
       <Route path='/Pizza' component={Form}/>
     </Switch>
-
+  </Router>
   );
 };
 export default App;
